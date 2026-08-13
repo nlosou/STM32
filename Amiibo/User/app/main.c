5a0c2988583a88dfc1698c63641657b068dad8e4
Noser <Noser_Official@outlook.com>
2026-08-13 20:24:12 +0800

    test:软件uart成功输出正确的数据:

--- Amiibo/User/app/main.c
+++ Amiibo/User/app/main.c
@@ -6,7 +6,7 @@
 int main(void)
 {   
     
-    uint16_t data = 0xAF;
+    uint16_t data = 0xAA;
     uart_msg_t msg = {
         .mode = Half_duplex,
         .parity_bits = parity_bits_off,
@@ -21,29 +21,13 @@ int main(void)
     uart_port_t *uart = bsp_get_uart_test();
     Timer_port_t *timer1 = bsp_get_tim();
 
-   // uart_init(uart);
-    gpio_port_init(gpio);
-   //exti_init(exti);
-    Timer_init(timer1);
+    uart_init(uart);
+    //exti_init(exti);
     nvic_init(nvic);
-    Timer_update_ck_cnt(timer1);
-    Timer_start(timer1);
-    //uart_transmit(uart,&msg,1);
-    //Timer_close(timer1);
+    //oled_point(oled);
+    uart_transmit(uart,&msg,1);
     while(1)
     {
-        //Timer_start(timer1);
-        if(flag)
-        {
-            gpio_port_write(gpio,1,GPIO_LEVEL_HIGH);
-         // flag = 0;
-        }
-        else
-        {
-            gpio_port_write(gpio,1,GPIO_LEVEL_LOW);
-        }
-        //OLED_ShowHex(0,0,flag);
-        //oled_point(oled);
     }
     return 0;
 }
