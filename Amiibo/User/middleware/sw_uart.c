03f5342ec9e8f1deb1df4c07130b53985ee1e5dd
Noser <Noser_Official@outlook.com>
2026-08-13 20:25:55 +0800

    fix:解决软件Uart会多读一个0少读1个1的bug,通过数组index解决的

--- Amiibo/User/middleware/sw_uart.c
+++ Amiibo/User/middleware/sw_uart.c
@@ -1,5 +1,5 @@
 #include "sw_uart.h"
-
+#include "oled.h"
 
 void sw_uart_init(void* ctx)
 {
@@ -7,7 +7,9 @@ void sw_uart_init(void* ctx)
    {
        return;
    }
+   
    sw_uart_ctx_t* sw_uart = (sw_uart_ctx_t*)(ctx);
+   //gpio_port_write(sw_uart->TX,sw_uart->TX_PIN,GPIO_LEVEL_HIGH);
    gpio_port_init(sw_uart->RX);
    gpio_port_init(sw_uart->TX);
    Timer_init(sw_uart->timer);
@@ -61,31 +63,24 @@ void sw_uart_send_data(void*ctx ,uint16_t transfer_data)
     sw_uart_ctx_t* sw_uart = (sw_uart_ctx_t*)(ctx);
     gpio_port_t* port = sw_uart->TX;
     uint16_t TX_PIN = sw_uart->TX_PIN;
-    //默认填入开始位
-    uint8_t tmep_bit[9] = {0};
-    
+    uint8_t tmep_bit[10] = {0};                      //默认填入开始位
+    tmep_bit[9] = 1;                                //填入停止位
+                                                    //
     for(uint8_t idx = 0 ; idx < 8 ; idx++)
     {
-        tmep_bit[idx + 1] = (transfer_data >>( idx - 1)) & 0x01 ;
+        tmep_bit[idx + 1] = (transfer_data >>idx) & 0x01 ;
     }
 
-    uint8_t idx = 0;
+    uint8_t idx = 0;   
     Timer_start(sw_uart->timer);
-    while(idx < 9)
+    while(idx < 10)
     {
         if(flag)
         {
             gpio_port_write(port,TX_PIN,tmep_bit[idx++]);
-        }
-        else
-        {
-            gpio_port_write(port,TX_PIN,tmep_bit[idx++]);
+            flag = 0;
         }
     }
-   //gpio_port_write(port,TX_PIN,GPIO_LEVEL_LOW);
-    //sw_uart_parity_bit(ctx);
-    //sw_uart_stop_bit(ctx);
-   gpio_port_write(port,TX_PIN,GPIO_LEVEL_HIGH);
 }
 
 uint8_t sw_uart_recieve_data(void*ctx ,uint8_t transfer_data)
@@ -99,7 +94,7 @@ uint8_t sw_uart_recieve_data(void*ctx ,uint8_t transfer_data)
     gpio_port_t* port = sw_uart->RX;
     uint16_t RX_PIN = sw_uart->RX_PIN;
 
-TODO://临时使用while
+//TODO:临时使用while
     while(gpio_port_read(port,RX_PIN) & 0x01)
     {
         for(uint8_t idx = 0 ; idx < 8 ; idx++)
